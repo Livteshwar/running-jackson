7b220dd0ce3b32d0f7b69c1662eac3cf22ba9e61
Livteshwar <84121496+Livteshwar@users.noreply.github.com>
2021-06-17 19:52:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,33 @@
-var jackson
-var jacksonImg
-var road
-var roadImg
-var boundry1
-var boundry2
+var jackson;
+var jacksonImg;
+var road;
+var roadImg;
+var boundry1;
+var boundry2;
 
 function preload(){
   //pre-load images
-  jacksonImg=loadAnimation("Runner-1.png,Runner-2.png")
-  roadImg=loadImage("path.png")
+  jacksonImg=loadAnimation("Runner-1.png","Runner-2.png");
+  roadImg=loadImage("path.png");
 }
 
 function setup(){
   createCanvas(400,400);
   //create sprites here
-  jackson= createSprite(200,100)
-  jackson.addAnimation("Running",jacksonImg)
-  road=createSprite(200,200)
-  road.addImage(roadImg)
-  road.velocityY=4
-  road.scale=1.2
-  boundry1=createSprite(10,200,5,400)
-  boundry1.visible=false
-  jackson.collide(boundry1)
-  boundry2=createSprite(390,200,5,400)
-  boundry2.visible=false
-  jackson.collide(boundry2)
+  road=createSprite(200,200);
+  road.addImage(roadImg);
+  road.velocityY=4;
+  road.scale=1.2;
+  boundry1=createSprite(60,200,5,400);
+  boundry1.visible=false;
+  
+  boundry2=createSprite(340,200,5,400);
+  boundry2.visible=false;
+  jackson= createSprite(200,200);
+  jackson.addAnimation("Running",jacksonImg);
+  jackson.scale=0.1
+  jackson.collide(boundry1);
+  jackson.collide(boundry2);
 }
 
 function draw() {
@@ -33,12 +35,8 @@ function draw() {
   if (road.y>400){
     road.y=height/2
   }
-  if (keydown("left")){
-    jackson.x -=10
-  }
-  if (keydown("right")){
-    jackson.x +=10
-  }
-
+  jackson.x=mouseX
+  drawSprites()
 }
+
 
